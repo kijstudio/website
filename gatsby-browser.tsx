593a0e7224f1450aab8ae7f03f660e74264b1946
Bartosz Grabski <b.grabski92@gmail.com>
2025-05-11 20:28:31 +0200

    fix: fix for ssr

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,22 @@
-import "./src/styles/global.css"
-import type { GatsbyBrowser } from "gatsby"
+import * as React from "react"
+import type { GatsbySSR } from "gatsby"
 
-export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({ element }) => {
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes,
+}) => {
+  setHtmlAttributes({ lang: "en" })
+  setBodyAttributes({ className: "my-body-class" })
+  setHeadComponents([
+    <link
+      key="typekit-font"
+      rel="stylesheet"
+      href="https://use.typekit.net/gzh3byk.css"
+    />,
+  ])
+}
+
+export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => {
   return element
 } 
